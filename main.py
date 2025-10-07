3e047a193ce3332ed0888a32049b3f924f9bd125
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-10-07 17:28:36 -0300

    Pass TDD 00100 `GET /vehicles` (list-1.0)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
+import re
 
 app = FastAPI(
     docs_url=None,   # Disables Swagger UI
@@ -9,8 +10,32 @@ app = FastAPI(
 )
 
 @app.get("/vehicles")
-async def vehicles_get():
-    return []
+async def vehicles_get(request: Request):
+    example = ''
+
+    if 'prefer' in request.headers:
+        match = re.match(r'example=([\w\.-]+)', request.headers.get("Prefer"))
+        if match:
+            example = match.group(1)
+
+    if 'empty' == example:
+        return []
+    elif 'list-1.0' == example:
+        return [
+            {
+                "id": 1,
+                "name": "pickup",
+                "volume": 1230000,
+                "volume_unit": "CUBIC_CENTIMETER",
+                "consumption": 12,
+                "consumption_unit": "LITERS_PER_100KM",
+                "category_type": "PICKUP",
+                "engine_type": "DIESEL"
+            }
+        ]
+
+    message = {"message": "Work In Progress"}
+    return JSONResponse(content=message, status_code=503)
 
 @app.post("/vehicles")
 async def vehicles_post():
