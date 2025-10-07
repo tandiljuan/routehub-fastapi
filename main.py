bc80340f77053a65ff830c24f920931ce1a101f0
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-10-07 17:24:15 -0300

    Pass TDD 00100 `POST /vehicles` (vehicle-1.0)

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ app = FastAPI(
 async def vehicles_get():
     return []
 
+@app.post("/vehicles")
+async def vehicles_post():
+    return {
+        "id": 1,
+        "name": "pickup",
+        "volume": 1230000,
+        "volume_unit": "CUBIC_CENTIMETER",
+        "consumption": 12,
+        "consumption_unit": "LITERS_PER_100KM",
+        "category_type": "PICKUP",
+        "engine_type": "DIESEL"
+    }
+
 @app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(request, exc):
     message = {"message": "Work In Progress"}
