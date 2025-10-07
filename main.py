0bae7ff4c3f72af30c5db53315a1a375a5418201
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-10-07 20:08:46 -0300

    Pass TDD 00100 `GET /vehicles/:id` (vehicle-1.1)

--- main.py
+++ main.py
@@ -64,17 +64,39 @@ async def vehicles_post():
     }
 
 @app.get("/vehicles/{id}")
-async def vehicles_id_get(id: int):
-    return {
-        "id": 1,
-        "name": "pickup",
-        "volume": 1230000,
-        "volume_unit": "CUBIC_CENTIMETER",
-        "consumption": 12,
-        "consumption_unit": "LITERS_PER_100KM",
-        "category_type": "PICKUP",
-        "engine_type": "DIESEL"
-    }
+async def vehicles_id_get(id: int, request: Request):
+    example = ''
+
+    if 'prefer' in request.headers:
+        match = re.match(r'example=([\w\.-]+)', request.headers.get("Prefer"))
+        if match:
+            example = match.group(1)
+
+    if 'vehicle-1.0' == example:
+        return {
+            "id": 1,
+            "name": "pickup",
+            "volume": 1230000,
+            "volume_unit": "CUBIC_CENTIMETER",
+            "consumption": 12,
+            "consumption_unit": "LITERS_PER_100KM",
+            "category_type": "PICKUP",
+            "engine_type": "DIESEL"
+        }
+    elif 'vehicle-1.1' == example:
+        return {
+            "id": 1,
+            "name": "pickup-update",
+            "volume": 1230000,
+            "volume_unit": "CUBIC_CENTIMETER",
+            "consumption": 10,
+            "consumption_unit": "LITERS_PER_100KM",
+            "category_type": "PICKUP",
+            "engine_type": "DIESEL"
+        }
+
+    message = {"message": "Work In Progress"}
+    return JSONResponse(content=message, status_code=503)
 
 @app.patch("/vehicles/{id}")
 async def vehicles_id_patch(id: int):
