fe3a19c646711b73f4866419bc994dd848f15ca7
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-10-11 17:55:40 -0300

    Pass TDD 00200 `GET /fleets/:id` (fleet-1.1)

--- routes/fleets.py
+++ routes/fleets.py
@@ -59,17 +59,39 @@ async def fleets_post():
     }
 
 @router.get("/{id}")
-async def fleets_id_get(id: int):
-    return {
-        "id": 1,
-        "name": "my-fleet",
-        "vehicles": [
-            {
-                "vehicle_id": 1,
-                "quantity": 2
-            }
-        ]
-    }
+async def fleets_id_get(id: int, request: Request):
+    example = ''
+
+    if 'prefer' in request.headers:
+        match = re.match(r'example=([\w\.-]+)', request.headers.get("Prefer"))
+        if match:
+            example = match.group(1)
+
+    if 'fleet-1.0' == example:
+        return {
+            "id": 1,
+            "name": "my-fleet",
+            "vehicles": [
+                {
+                    "vehicle_id": 1,
+                    "quantity": 2
+                }
+            ]
+        }
+    elif 'fleet-1.1' == example:
+        return {
+            "id": 1,
+            "name": "my-fleet-update",
+            "vehicles": [
+                {
+                    "vehicle_id": 2,
+                    "quantity": 3
+                }
+            ]
+        }
+
+    message = {"message": "Work In Progress"}
+    return JSONResponse(content=message, status_code=503)
 
 @router.patch("/{id}")
 async def fleets_id_patch(id: int):
