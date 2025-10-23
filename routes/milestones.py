1368c9f937db876f4dc1b47ab2a07e3c10de1a99
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-10-23 20:56:32 -0300

    Pass TDD 00400 `GET /milestones` (list-1.0)

--- routes/milestones.py
+++ routes/milestones.py
@@ -1,10 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
+import re
 
 router = APIRouter(prefix="/milestones")
 
 @router.get("")
-async def milestones_get():
-    return []
+async def milestones_get(request: Request):
+    example = ''
+
+    if 'prefer' in request.headers:
+        match = re.match(r'example=([\w\.-]+)', request.headers.get("Prefer"))
+        if match:
+            example = match.group(1)
+
+    if 'empty' == example:
+        return []
+    elif 'list-1.0' == example:
+        return [
+            {
+                "id": 1,
+                "name": "Main Depot",
+                "location": "37.7749,-122.4194",
+                "milestone_category": "DEPOT"
+            }
+        ]
+
+    message = {"message": "Work In Progress"}
+    return JSONResponse(content=message, status_code=503)
 
 @router.post("")
 async def milestones_post():
