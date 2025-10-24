0097ec8a76da4bc0f0bfba9ac53125f9b4a6a122
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-10-24 17:23:51 -0300

    Pass TDD 00500 `GET /deliveries/:id` (delivery-1.0)

--- routes/deliveries.py
+++ routes/deliveries.py
@@ -54,3 +54,21 @@ async def deliveries_post():
         "weight": 1000,
         "weight_unit": "GRAMS"
     }
+
+@router.get("/{id}")
+async def deliveries_id_get(id: int):
+    return {
+        "id": 1,
+        "delivery_method": "DELIVERY",
+        "milestone_id": 1,
+        "destination": "37.7749,-122.4194",
+        "schedule": ["DTSTART:20240704T120000Z"],
+        "width": 10,
+        "height": 20,
+        "depth": 30,
+        "length_unit": "CENTIMETER",
+        "volume": 6000,
+        "volume_unit": "CUBIC_CENTIMETER",
+        "weight": 1000,
+        "weight_unit": "GRAMS"
+    }
