cbd2954435a6fa3e7f9ae94e72937a66387daa2e
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-10-26 18:30:03 -0300

    Pass TDD 00600 `POST /lots` (delivery-lot-1.0)

--- routes/delivery_lots.py
+++ routes/delivery_lots.py
@@ -5,3 +5,14 @@ router = APIRouter(prefix="/lots")
 @router.get("")
 async def delivery_lots_get():
     return []
+
+@router.post("")
+async def delivery_lots_post():
+    return {
+        "id": 1,
+        "milestone_id": 1,
+        "deliveries": [1],
+        "fleet_id": 1,
+        "drivers": [1],
+        "state": "UNPROCESSED"
+    }
