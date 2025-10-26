a52da587e148b0862312c422edb4ea7e63f0dec5
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-10-26 18:39:17 -0300

    Pass TDD 00600 `GET /lots/:id` (delivery-lot-1.0)

--- routes/delivery_lots.py
+++ routes/delivery_lots.py
@@ -40,3 +40,14 @@ async def delivery_lots_post():
         "drivers": [1],
         "state": "UNPROCESSED"
     }
+
+@router.get("/{id}")
+async def delivery_lots_id_get(id: int):
+    return {
+        "id": 1,
+        "milestone_id": 1,
+        "deliveries": [1],
+        "fleet_id": 1,
+        "drivers": [1],
+        "state": "UNPROCESSED"
+    }
