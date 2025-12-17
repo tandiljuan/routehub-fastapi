194fac2c61572974f5be7b08bbb2edd6005c7a3f
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-12-17 19:02:08 -0300

    feat(routes): implement `POST /vehicles` transactional logic

--- routes/vehicles.py
+++ routes/vehicles.py
@@ -1,6 +1,15 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import JSONResponse
 import re
+from fastapi import (
+    APIRouter,
+    Request,
+)
+from fastapi.responses import JSONResponse
+from models.database import Session as DbSession
+from models.vehicle import (
+    Vehicle,
+    VehicleCreate,
+    VehicleResponse,
+)
 
 router = APIRouter(prefix="/vehicles")
 
@@ -45,18 +54,20 @@ async def vehicles_get(request: Request):
     message = {"message": "Work In Progress"}
     return JSONResponse(content=message, status_code=503)
 
-@router.post("")
-async def vehicles_post():
-    return {
-        "id": 1,
-        "name": "pickup",
-        "volume": 1230000,
-        "volume_unit": "CUBIC_CENTIMETER",
-        "consumption": 12,
-        "consumption_unit": "LITERS_PER_100KM",
-        "category_type": "PICKUP",
-        "engine_type": "DIESEL"
-    }
+@router.post(
+    "",
+    response_model=VehicleResponse,
+    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
+)
+async def vehicles_post(db: DbSession, post_data: VehicleCreate):
+    veh_dict = post_data.model_dump()
+    veh_dict['company_id'] = 1
+    veh_db = Vehicle.model_validate(veh_dict)
+    db.add(veh_db)
+    db.commit()
+    db.refresh(veh_db)
+    return veh_db
 
 @router.get("/{id}")
 async def vehicles_id_get(id: int, request: Request):
