526a4e913c15c010383bc8f4695a5590edb267f0
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-12-20 17:41:19 -0300

    feat(models): add `FleetResponse` model

--- models/fleet.py
+++ models/fleet.py
@@ -1,4 +1,6 @@
+from pydantic import field_serializer
 from sqlmodel import SQLModel
+from .vehicle import VehicleResponse
 
 class FleetBase(SQLModel):
     name: str
@@ -9,3 +11,14 @@ class FleetVehicleCreate(SQLModel):
 
 class FleetCreate(FleetBase):
     vehicles: list[FleetVehicleCreate]
+
+class FleetVehicleResponse(VehicleResponse):
+    qty: int
+
+class FleetResponse(FleetCreate):
+    id: str | int
+    vehicles: list[FleetVehicleResponse]
+
+    @field_serializer('id', when_used='json')
+    def serialize_id_to_str(self, id: int):
+        return str(id)
