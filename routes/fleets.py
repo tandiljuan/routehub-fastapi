761e9f2d5b08459ba8c3bd65f351a570b9382606
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-12-21 16:21:33 -0300

    feat(routes): implement `POST /fleets` transactional logic

--- routes/fleets.py
+++ routes/fleets.py
@@ -4,9 +4,13 @@ from fastapi import (
     Request,
 )
 from fastapi.responses import JSONResponse
+from models.database import Session as DbSession
+from models.vehicle import Vehicle
 from models.fleet import (
+    Fleet,
     FleetCreate,
     FleetResponse,
+    FleetVehicle,
 )
 
 router = APIRouter(prefix="/fleets")
@@ -58,17 +62,39 @@ async def fleets_get(request: Request):
     response_model_exclude_unset=True,
     response_model_exclude_none=True,
 )
-async def fleets_post(post_data: FleetCreate):
-    return {
-        "id": 1,
-        "name": "my-fleet",
-        "vehicles": [
-            {
-                "vehicle_id": 1,
-                "quantity": 2
-            }
-        ]
-    }
+async def fleets_post(db: DbSession, post_data: FleetCreate):
+    # Add Company ID to submitted data
+    flt_dict = post_data.model_dump()
+    flt_dict['company_id'] = 1
+    flt_db = Fleet.model_validate(flt_dict)
+
+    # Create Fleet
+    db.add(flt_db)
+    db.commit()
+
+    # Create relations between Fleet and Vehicles
+    post_vehicles = post_data.vehicles or []
+    for v in post_data.vehicles:
+        veh_id = int(v.id)
+        veh_qty = int(v.qty)
+
+        if veh_qty < 1:
+            continue
+
+        veh_db = db.get(Vehicle, veh_id)
+        if veh_db:
+            link = FleetVehicle(
+                fleet=flt_db,
+                vehicle=veh_db,
+                quantity=veh_qty,
+            )
+            flt_db.vehicles.append(link)
+            db.add(flt_db)
+            db.commit()
+
+    # Return (custom serialized) Fleet
+    db.refresh(flt_db)
+    return flt_db.model_dump()
 
 @router.get("/{id}")
 async def fleets_id_get(id: int, request: Request):
