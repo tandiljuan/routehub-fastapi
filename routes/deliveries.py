dfe46d958e9531abb4ff11c4a1edd4e7e9ff47ad
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-12-28 21:41:03 -0300

    feat(routes): implement `GET /deliveries` transactional logic

--- routes/deliveries.py
+++ routes/deliveries.py
@@ -5,6 +5,7 @@ from fastapi import (
     Response,
 )
 from fastapi.responses import JSONResponse
+from sqlmodel import select
 from models.database import Session as DbSession
 from models.delivery import (
     Delivery,
@@ -14,56 +15,15 @@ from models.delivery import (
 
 router = APIRouter(prefix="/deliveries")
 
-@router.get("")
-async def deliveries_get(request: Request):
-    example = ''
-
-    if 'prefer' in request.headers:
-        match = re.match(r'example=([\w\.-]+)', request.headers.get("Prefer"))
-        if match:
-            example = match.group(1)
-
-    if 'empty' == example:
-        return []
-    elif 'list-1.0' == example:
-        return [
-            {
-                "id": 1,
-                "delivery_method": "DELIVERY",
-                "milestone_id": 1,
-                "destination": "37.7749,-122.4194",
-                "schedule": ["DTSTART:20240704T120000Z"],
-                "width": 10,
-                "height": 20,
-                "depth": 30,
-                "length_unit": "CENTIMETER",
-                "volume": 6000,
-                "volume_unit": "CUBIC_CENTIMETER",
-                "weight": 1000,
-                "weight_unit": "GRAMS"
-            }
-        ]
-    elif 'list-1.1' == example:
-        return [
-            {
-                "id": 1,
-                "delivery_method": "DELIVERY",
-                "milestone_id": 1,
-                "destination": "37.7749,-122.4194",
-                "schedule": ["DTSTART:20240704T120000Z"],
-                "width": 15,
-                "height": 25,
-                "depth": 35,
-                "length_unit": "CENTIMETER",
-                "volume": 6500,
-                "volume_unit": "CUBIC_CENTIMETER",
-                "weight": 1500,
-                "weight_unit": "GRAMS"
-            }
-        ]
-
-    message = {"message": "Work In Progress"}
-    return JSONResponse(content=message, status_code=503)
+@router.get(
+    "",
+    response_model=list[DeliveryResponse],
+    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
+)
+async def deliveries_get(db: DbSession):
+    dlv_list = db.exec(select(Delivery)).all()
+    return dlv_list
 
 @router.post(
     "",
