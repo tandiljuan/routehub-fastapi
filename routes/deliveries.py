c78d1c18a2acabade93bd98d299c9ca1b841a1e5
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-12-28 21:45:52 -0300

    feat(routes): implement `GET /deliveries/{id}` transactional logic

--- routes/deliveries.py
+++ routes/deliveries.py
@@ -1,10 +1,9 @@
-import re
 from fastapi import (
     APIRouter,
+    HTTPException,
     Request,
     Response,
 )
-from fastapi.responses import JSONResponse
 from sqlmodel import select
 from models.database import Session as DbSession
 from models.delivery import (
@@ -51,56 +50,15 @@ async def deliveries_post(
 @router.get(
     "/{id}",
     name="deliveries_id_get",
+    response_model=DeliveryResponse,
+    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
 )
-async def deliveries_id_get(id: int, request: Request):
-    key = ''
-    example = ''
-
-    if 'prefer' in request.headers:
-        match = re.match(r'(\w+)=([\w\.-]+)', request.headers.get("Prefer"))
-        if match:
-            key = match.group(1)
-            example = match.group(2)
-
-    if 'code' == key:
-        message = {"message": "..."}
-        return JSONResponse(content=message, status_code=int(example))
-
-    if 'delivery-1.0' == example:
-        return {
-            "id": 1,
-            "delivery_method": "DELIVERY",
-            "milestone_id": 1,
-            "destination": "37.7749,-122.4194",
-            "schedule": ["DTSTART:20240704T120000Z"],
-            "width": 10,
-            "height": 20,
-            "depth": 30,
-            "length_unit": "CENTIMETER",
-            "volume": 6000,
-            "volume_unit": "CUBIC_CENTIMETER",
-            "weight": 1000,
-            "weight_unit": "GRAMS"
-        }
-    elif 'delivery-1.1' == example:
-        return {
-            "id": 1,
-            "delivery_method": "DELIVERY",
-            "milestone_id": 1,
-            "destination": "37.7749,-122.4194",
-            "schedule": ["DTSTART:20240704T120000Z"],
-            "width": 15,
-            "height": 25,
-            "depth": 35,
-            "length_unit": "CENTIMETER",
-            "volume": 6500,
-            "volume_unit": "CUBIC_CENTIMETER",
-            "weight": 1500,
-            "weight_unit": "GRAMS"
-        }
-
-    message = {"message": "Work In Progress"}
-    return JSONResponse(content=message, status_code=503)
+async def deliveries_id_get(id: int, db: DbSession):
+    dlv_db = db.get(Delivery, id)
+    if not dlv_db:
+        raise HTTPException(status_code=404, detail="Delivery not found")
+    return dlv_db
 
 @router.patch("/{id}")
 async def deliveries_id_patch(id: int):
