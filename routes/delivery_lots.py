36c082668ce944b2d08502fbf9d115aff32e4b32
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-12-31 16:00:24 -0300

    feat(routes): implement `POST /lots` transactional logic

--- routes/delivery_lots.py
+++ routes/delivery_lots.py
@@ -4,10 +4,16 @@ from fastapi import (
     Request,
 )
 from fastapi.responses import JSONResponse
+from models.database import Session as DbSession
+from models.delivery import Delivery
 from models.delivery_lot import (
+    DeliveryLot,
     DeliveryLotCreate,
+    DeliveryLotDelivery,
+    DeliveryLotDriver,
     DeliveryLotResponse,
 )
+from models.driver import Driver
 
 router = APIRouter(prefix="/lots")
 
@@ -54,15 +60,51 @@ async def delivery_lots_get(request: Request):
     response_model_exclude_unset=True,
     response_model_exclude_none=True,
 )
-async def delivery_lots_post(post_data: DeliveryLotCreate):
-    return {
-        "id": 1,
-        "milestone_id": 1,
-        "deliveries": [1],
-        "fleet_id": 1,
-        "drivers": [1],
-        "state": "UNPROCESSED"
-    }
+async def delivery_lots_post(
+    db: DbSession,
+    post_data: DeliveryLotCreate,
+):
+    # Add Company ID to submitted data
+    lot_dict = post_data.model_dump()
+    lot_dict['company_id'] = 1
+    lot_dict = DeliveryLot.normalize_submitted_dict(lot_dict)
+    lot_db = DeliveryLot.model_validate(lot_dict)
+
+    # Create Lot
+    db.add(lot_db)
+    db.commit()
+
+    # Create relations between Lot and Deliveries
+    post_deliveries = post_data.deliveries or []
+    for dlv_id in post_deliveries:
+        dlv_id = int(dlv_id)
+        dlv_db = db.get(Delivery, dlv_id)
+        if dlv_db:
+            link = DeliveryLotDelivery(
+                lot=lot_db,
+                delivery=dlv_db,
+            )
+            lot_db.deliveries.append(link)
+            db.add(lot_db)
+            db.commit()
+
+    # Create relations between Lot and Drivers
+    post_drivers = post_data.drivers or []
+    for drv_id in post_drivers:
+        drv_id = int(drv_id)
+        drv_db = db.get(Driver, drv_id)
+        if drv_db:
+            link = DeliveryLotDriver(
+                lot=lot_db,
+                driver=drv_db,
+            )
+            lot_db.drivers.append(link)
+            db.add(lot_db)
+            db.commit()
+
+    # Return (custom serialized) Lot
+    db.refresh(lot_db)
+    return lot_db.model_dump()
 
 @router.get("/{id}")
 async def delivery_lots_id_get(id: int, request: Request):
