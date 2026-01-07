378bb2a4851deca5837312c368a02c0877457564
Juan Manuel Lopez <git.nrbye@slmail.me>
2026-01-07 16:38:31 -0300

    feat(optimizer): implement logic for submitting plans

--- libs/optimizer/optimizer.py
+++ libs/optimizer/optimizer.py
@@ -1,6 +1,19 @@
+import json
+import requests
+from .models import PlanContext
+
 class Optimizer():
 
     def __init__(self, host: str, port: int, auth: str = None):
         self.host = host
         self.port = port
         self.auth = auth
+
+    def send_route_plan(self, plan: PlanContext) -> str:
+        payload = plan.model_dump(serialize_as_any=True)
+        payload = json.dumps(payload)
+        url = f"{self.host}:{self.port}/route-optimizer-app/routes"
+        headers = {'api-key': self.auth} if self.auth else {}
+        r = requests.post(url, data=payload, headers=headers)
+        rbody = json.loads(r.text)
+        return rbody['session_id']
