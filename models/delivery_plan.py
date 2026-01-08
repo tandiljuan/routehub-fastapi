bd119d638f1e979471d3ef383437bacf28ada797
Juan Manuel Lopez <git.nrbye@slmail.me>
2026-01-08 17:15:20 -0300

    feat(models): add `DeliveryPath` transactional model

--- models/delivery_plan.py
+++ models/delivery_plan.py
@@ -4,6 +4,9 @@ from sqlmodel import (
     SQLModel,
 )
 from .delivery_lot import DeliveryLot
+from .driver import Driver
+from .milestone import Milestone
+from .vehicle import Vehicle
 
 class DeliveryPlan(SQLModel, table=True):
     __tablename__ = "delivery_plan"
@@ -13,3 +16,12 @@ class DeliveryPlan(SQLModel, table=True):
     optimizer_id: str
 
     lot: DeliveryLot = Relationship()
+
+class DeliveryPath(SQLModel, table=True):
+    __tablename__ = "delivery_path"
+
+    id: int | None = Field(default=None, primary_key=True)
+    delivery_plan_id: int = Field(foreign_key="delivery_plan.id")
+    milestone_id: int = Field(foreign_key="milestone.id")
+    vehicle_id: int | None = Field(default=None, foreign_key="vehicle.id")
+    driver_id: int | None = Field(default=None, foreign_key="driver.id")
